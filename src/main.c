ce0c54059e00a3c949eead625d77a69b288d123b
Jin Yang <redus891@gmail.com>
2015-06-08 01:50:02 +0900

    revert to referring localtime on load time

--- src/main.c
+++ src/main.c
@@ -115,6 +115,10 @@ static void main_window_load(Window *window){
 	text_layer_set_font(s_day_layer, s_small_font);
 	text_layer_set_text_alignment(s_day_layer, GTextAlignmentCenter);
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_day_layer));
+	
+	// load time
+	time_t temp = time(NULL);
+ 	tick_handler(localtime(&temp), SECOND_UNIT);
 }
 
 static void main_window_unload(Window *window){
